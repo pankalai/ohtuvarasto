9d6d9b7d6c698dd76c4537c173ed39cd9eb56256
Anssi Pänkäläinen <anssi.pankalainen@gmail.com>
2023-11-08 19:07:00 +0200

    pylint test

--- src/varasto.py
+++ src/varasto.py
@@ -3,7 +3,7 @@ class Varasto:
         if tilavuus > 0.0:
             self.tilavuus = tilavuus
         else:
-            # virheellinen, nollataan
+            # virheellinen, nollataa
             self.tilavuus = 0.0
 
         if alku_saldo < 0.0:
@@ -16,7 +16,8 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
